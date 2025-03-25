8d3503e555150543846dd15349a4281ff28efc1d
ichikawa <68413973+yrichika@users.noreply.github.com>
2025-03-25 16:18:29 +0900

    add service adapter

--- internal/module/user/service/user_service.go
+++ internal/module/user/service/user_service.go
@@ -1,1 +1,7 @@
 package service
+
+// サービスを別のモジュールから使う場合は、
+// 直接このサービスを呼び出すのではなく、
+// 一度ServiceAdapterを通して呼び出すこと
+type UserService interface {
+}
---
+++ internal/service_adapter/user/user_sa.go
@@ -0,0 +1,16 @@
+package user
+
+import (
+	"github.com/nolafw/projecttemplate/internal/module/user/service"
+)
+
+// 別のモジュールのserviceを、いったんこの
+// ServiceAdapterを通して呼び出すことで、
+// そのserviceの実装を隠蔽することができる
+// こうすることで、最初はmodule/serviceをただラップして使っていたとして
+// 後でマイクロサービスに移行した場合に、このserviceadapterの中で
+// module/serviceを呼び出すのではなく、gRPCなどでマイクロサービスを呼び出すように変更するだけで済む
+// ここは、モノリスからマイクロサービスに移行しやすくするための中間の層としての役割を持つ
+type UserSA struct {
+	userService service.UserService
+}
