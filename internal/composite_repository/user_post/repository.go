2e1b302f1e7977d4b730c3a1de1ce579703986d7
ichikawa <68413973+yrichika@users.noreply.github.com>
2025-03-31 19:37:24 +0900

    refactor table mapper ideas to CQRS ideas

---
+++ internal/composite_repository/user_post/repository.go
@@ -0,0 +1,3 @@
+package userpost
+
+type UserPostRepository interface{}
--- internal/module/user/entity/user_entity.go
+++ internal/module/user/entity/user_entity.go
@@ -1,1 +1,4 @@
 package entity
+
+// module内のdbのデータは、`***Entity`という名前にして
+// CQRSのQuery(composite_repository)の、Modelとは区別したものとする。
