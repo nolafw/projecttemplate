9dd7100c0c2d4bce1d75d5f95c3a29d23a0a90e4
ichikawa <68413973+yrichika@users.noreply.github.com>
2025-05-08 19:45:16 +0900

    implement scheduler flag to run as a scheduler in main function

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 
 	"github.com/nolafw/projecttemplate/internal"
+	"github.com/nolafw/projecttemplate/internal/scheduler"
 )
 
 // main entry point
@@ -11,10 +12,15 @@ import (
 func main() {
 
 	env := flag.String("e", "local", "it must be either [local], [develop], [staging], [production] or [testing].")
+	isScheduler := flag.Bool("scheduler", false, "if true, run as scheduler.")
 	flag.Parse()
 	if env == nil {
 		panic("Please specify the environment with -e option. It must be either [local], [develop], [staging], [production] or [testing].")
 	}
 
-	internal.Run(env)
+	if isScheduler != nil && *isScheduler {
+		scheduler.Start(env)
+	} else {
+		internal.Run(env)
+	}
 }
