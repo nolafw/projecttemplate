c4199800ea6507614b70a7949e7b9eb05cef3ae4
ichikawa <68413973+yrichika@users.noreply.github.com>
2025-09-03 08:20:53 +0900

    feat: change entity folder names to model

--- internal/module/user/entity/user_entity.go
+++
@@ -1,4 +0,0 @@
-package entity
-
-// module内のdbのデータは、`***Entity`という名前にして
-// CQRSのQuery(composite_repository)の、Modelとは区別したものとする。
---
+++ internal/module/user/model/user_entity.go
@@ -0,0 +1,4 @@
+package entity
+
+// Entityという名前だと、すべてのフィールドをValue Objectにするイメージがある
+// そこまで厳密でないものにしたいため、modelにする
